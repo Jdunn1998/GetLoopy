045a33082eea5f7ff1a31f7507543753773ac544
jdunn1998 <dunn2jm@mail.uc.edu>
2025-03-02 13:30:53 -0500

    Finished Task 6

--- src/Task6.java
+++ src/Task6.java
@@ -1,2 +1,11 @@
 public class Task6 {
+    public static void main(String[] args) {
+
+        for(int i = 5; i >= 1; i = i - 1){
+            for(int j = 1; j <= i; j= j + 1){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
 }
