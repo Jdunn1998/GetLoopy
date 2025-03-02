9cbf8607a66e45603a9281aad1f02d8ea1701c0e
jdunn1998 <dunn2jm@mail.uc.edu>
2025-03-02 13:14:47 -0500

    Finished Task 4

--- src/Task4.java
+++ src/Task4.java
@@ -1,6 +1,6 @@
 public class Task4 {
     public static void main(String[] args) {
 
-        for (int i = 0; i <= 18; i = i + 3) {
+        for (int i = 10; i >= 0; i = i - 2) {
             System.out.println(i);
 }}}
